6e98559b497de8741b490f41bfe607e68ec86cea
ftp <a99953zd_slv@donald.beget.ru>
2020-03-20 12:02:20 +0300

    ...

--- wp-content/themes/storefront-child/inc/woocommerce/storefront-woocommerce-template-hooks.php
+++ wp-content/themes/storefront-child/inc/woocommerce/storefront-woocommerce-template-hooks.php
@@ -135,7 +135,11 @@ remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_re
 
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
 
-add_action('woocommerce_after_single_product_summary', 'woocommerce_template_single_price', 10);
+//add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 25);
+add_action('woocommerce_single_variation', 'woocommerce_template_single_price', 15);
+
+remove_action('woocommerce_single_variation', 'woocommerce_single_variation', 10);
+//add_action('woocommerce_after_single_product_summary', 'woocommerce_template_single_price', 10);
 
 //remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50);
 
